83a0ade5d6b76e14e1364b01217ff2b7565c7231
Gabriel Karbouch <gakarbou@c2r5p3.42perpignan.fr>
2025-01-22 16:40:43 +0100

    wtf

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 20:53:26 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/01/21 18:18:48 by gakarbou         ###   ########.fr       */
+/*   Updated: 2025/01/22 16:39:10 by gakarbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,44 +28,46 @@ long long	ft_round(double nb)
 	return (nb / 1);
 }
 
-double	time_diff(struct timeval start, struct timeval cur)
+long long	time_diff(struct timeval start, struct timeval cur)
 {
 	double	a;
-	double 	b;
+	double	b;
 
-	a = (double)cur.tv_usec / 1000;
 	b = (double)start.tv_usec / 1000;
+	a = (double)cur.tv_usec / 1000 + (1000 * (cur.tv_sec - start.tv_sec));
+	printf("A = %lf, B = %lf\n", a, b);
 	return (a - b);
 }
 
-void	*test(void *ptr_start)
+void	*routine(void *ptr_start)
 {
-	static	pthread_mutex_t	lck;
+	static pthread_mutex_t	lck;
 	struct timeval cur;
+	static int	a = 0;
 	t_haha			*haha;
 
 	haha = (t_haha *)ptr_start;
 	while (!haha->aled)
 		;
 	pthread_mutex_lock(&lck);
-	usleep(10000);
+	usleep(1000000);
 	gettimeofday(&cur, NULL);
 	pthread_mutex_unlock(&lck);
-	printf("%lld\n", ft_round(time_diff(haha->start, cur)));
+	printf("%d finished at %lld\n", a++, time_diff(haha->start, cur));
 }
 
 int	main(void)
 {
 	int			i;
-	pthread_t	id[500];
+	pthread_t	id[5];
 	t_haha		haha;
 
 	i = -1;
 	haha.aled = 0;
 	while (++i < 5)
-		pthread_create(&(id[i]), NULL, &test, &haha);
+		pthread_create(&(id[i]), NULL, &routine, &haha);
 	usleep(500);
-	gettimeofday(&(haha.start), NULL);
+	gettimeofday(&haha.start, NULL);
 	haha.aled = 1;
 	i = -1;
 	while (++i < 5)
